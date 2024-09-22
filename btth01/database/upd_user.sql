34ba3021234eb54c8a88403b4b6375ad4700a236
scorpion226 <nguyenhieu22062004@gmail.com>
2024-09-22 23:57:17 +0700

    #

--- btth01/database/upd_user.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO Users (username, password, role) VALUES
-('user1', 'congngheweb', 'user'),
-('admin1', 'thayhieudeptrai', 'admin'),
-('user2', 'huhuhu', 'user'),
-('mhieu', 'nguyenminhhieu', 'admin'),
-('panh', 'nguyenthiphuonganh', 'user');
